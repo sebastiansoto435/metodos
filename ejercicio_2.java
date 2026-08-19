39a027d10663084f17182237b2f85ff703ad457b
Sebastian soto <juancarloshj25@gmail.com>
2026-08-19 09:47:35 -0500

    ejercicio 2 completado

--- ejercicio_2.java
+++ ejercicio_2.java
@@ -1,9 +1,10 @@
 public class ejercicio_2 {
     public static void main(String[] args) {
-
+        saludarEstudiante("carlos",25);
     }
 
-
-
+    public static void saludarEstudiante(String nombre, int edad) {
+        System.out.println("Hola " + nombre + ", tienes " + edad + " años.");
+    }
 
 }
