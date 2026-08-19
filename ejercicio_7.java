93d080e572f6960b1aa770786e54ac2ff871d925
Sebastian soto <juancarloshj25@gmail.com>
2026-08-19 10:28:04 -0500

    ejercicio 7 completado

--- ejercicio_7.java
+++ ejercicio_7.java
@@ -1,9 +1,18 @@
 public class ejercicio_7 {
     public static void main(String[] args) {
-
+        double[] calificaciones = {4.0, 4.5, 5.0, 4.5};
+        System.out.println("El promedio del grupo es: " + calcularPromediosClase(calificaciones));
     }
 
+    public static double calcularPromediosClase(double[] calificaciones) {
+        double suma = 0;
 
+        for (double nota : calificaciones) {
+            suma += nota;
+        }
+
+        return suma / calificaciones.length;
+    }
 
 
 }
