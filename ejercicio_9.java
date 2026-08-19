e67a6a5cc01fa0b6586b8ca25a98b2f40c07ade0
Sebastian soto <juancarloshj25@gmail.com>
2026-08-19 10:50:02 -0500

    ejercicio 9 completado

--- ejercicio_9.java
+++ ejercicio_9.java
@@ -1,9 +1,17 @@
 public class ejercicio_9 {
     public static void main(String[] args) {
-
+        System.out.println(clasificarNota(2.1));
     }
 
-
+    public static String clasificarNota(double nota){
+        if (nota <= 2.9){
+            return "reprobado";
+        }else if (nota <= 4.0){
+            return "aprobado";
+        }else{
+            return "Excelente";
+        }
+    }
 
 
 }
