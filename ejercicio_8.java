8f49a4b08d1b1e4c0bb79508ca1254b06d7be12a
Sebastian soto <juancarloshj25@gmail.com>
2026-08-19 10:38:04 -0500

    ejercicio 8 completado

--- ejercicio_8.java
+++ ejercicio_8.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class ejercicio_8 {
     public static void main(String[] args) {
-
+        for (int resultado : generarTablaMultiplicar(8)) {
+            System.out.println(resultado);
+        }
     }
 
+    public static int[] generarTablaMultiplicar(int numero){
+        int[] tabla = new int[10];
+        for (int i = 0; i < 10; i++) {
+            tabla[i] = numero * (i + 1);
+        }
 
+        return tabla;
+    }
 
 
 }
