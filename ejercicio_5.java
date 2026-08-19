dfe7eef7a7dc9490ab2a928a583c688dc14b1cf3
Sebastian soto <juancarloshj25@gmail.com>
2026-08-19 10:22:23 -0500

    ejercicio 5 completado

--- ejercicio_5.java
+++ ejercicio_5.java
@@ -1,9 +1,26 @@
+import javax.swing.*;
+
 public class ejercicio_5 {
     public static void main(String[] args) {
 
+        System.out.println(calcularPromedio(5.0,3.0));
+
+        double[] notas = {4.0, 4.5, 5.0, 4.5};
+        System.out.println(calcularPromedios(notas));
     }
 
+    public static double calcularPromedio(double nota1, double nota2){
+        return (nota1 + nota2) / 2;
+    }
+    public static double calcularPromedios(double[] notas) {
+        double suma = 0;
 
+        for (double nota : notas) {
+            suma += nota;
+        }
+
+        return suma / notas.length;
+    }
 
 
 }
