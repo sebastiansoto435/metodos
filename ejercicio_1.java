944f116dbd4c9229c99007090a7c5eff127afb14
Sebastian soto <juancarloshj25@gmail.com>
2026-08-19 09:39:59 -0500

    ejercicio 1 completado

--- ejercicio_1.java
+++ ejercicio_1.java
@@ -1,8 +1,11 @@
 public class ejercicio_1 {
     public static void main(String[] args) {
-
+        mostrarBienvenida();
     }
 
+    public static void mostrarBienvenida() {
+        System.out.println("Bienvenido al instituto");
+    }
 
 
 
