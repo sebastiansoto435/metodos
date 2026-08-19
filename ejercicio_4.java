1e2093a347e8a7cb5a28af09bd2bb4ba3d438b39
Sebastian soto <juancarloshj25@gmail.com>
2026-08-19 09:56:45 -0500

    ejercicio 4 completado

--- ejercicio_4.java
+++ ejercicio_4.java
@@ -1,9 +1,15 @@
 public class ejercicio_4 {
     public static void main(String[] args) {
-
+        System.out.println(esPar(2));
     }
 
-
+    public static boolean esPar(int numero){
+        if(numero%2==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 
 }
