02e63495c78c3ca1701fe3cbdd89b2a4c0b06843
Sebastian soto <juancarloshj25@gmail.com>
2026-08-19 10:56:23 -0500

    ejercicio 5 modificado

--- ejercicio_5.java
+++ ejercicio_5.java
@@ -6,13 +6,13 @@ public class ejercicio_5 {
         System.out.println(calcularPromedio(5.0,3.0));
 
         double[] notas = {4.0, 4.5, 5.0, 4.5};
-        System.out.println(calcularPromedios(notas));
+        System.out.println(calcularPromedio(notas));
     }
 
     public static double calcularPromedio(double nota1, double nota2){
         return (nota1 + nota2) / 2;
     }
-    public static double calcularPromedios(double[] notas) {
+    public static double calcularPromedio(double[] notas) {
         double suma = 0;
 
         for (double nota : notas) {
