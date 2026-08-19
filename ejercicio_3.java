13ee0bbe53eb473c76ffb42bc86fb8caeea497cd
Sebastian soto <juancarloshj25@gmail.com>
2026-08-19 09:53:03 -0500

    ejercicio 3 completado

--- ejercicio_3.java
+++ ejercicio_3.java
@@ -1,9 +1,11 @@
 public class ejercicio_3 {
     public static void main(String[] args) {
-
+        System.out.println("El area del rectangulo es: " + calcularAreaRectangulo(25,30));
     }
 
-
+    public static double calcularAreaRectangulo(double base, double altura){
+        return base*altura;
+    }
 
 
 }
